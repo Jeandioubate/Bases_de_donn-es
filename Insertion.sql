7d334cd48449688943da489bdc3dd43a64a4114a
Jean Dioubaté <jean.dioubate@fms-ea.com>
2025-11-28 12:54:57 +0100

    correction fichier

--- Insertion.sql
+++ Insertion.sql
@@ -1,6 +1,8 @@
 
 -- Insertion dans les tables --
 
+-- EXO 1 --
+
 INSERT INTO `g_article` (`a_reference`, `a_designation`, `a_marque`, `a_quantite`) VALUES
 ('6464641', 'filtre à air', 'MAN FILTER', 120),
 ('6464642', 'filtre pollen', 'BOSCH', 52),
@@ -22,6 +24,8 @@ INSERT INTO `g_voiture` (`v_marque`, `v_type`, `v_energie`, `v_fk_article_id`) V
 ('CITROEN', 'C3', 'ESSENCE', 6),
 ('PEUGEOT', '308', 'DIESEL', 7),
 ('MERCEDES', 'A180', 'DIESEL', 8);
+
+-- EXO 2 --
 
 INSERT INTO `g_client` (`c_nom`, `c_num_telephone`, `c_adresse`) VALUES
 ('Martin', '01 23 45 67 89', '15 Rue de la République, 75001 Paris'),
@@ -52,3 +56,17 @@ INSERT INTO `g_ordre` (`o_date`, `o_temps`,`o_montant`, `o_fk_client_id`) VALUES
 ('2024-03-05', '01:30:00', '180.75', 6),  
 ('2024-03-25', '05:00:00', '625.80', 7),
 ('2024-04-05', '02:20:00', '310.25', 8); 
+
+-- EXO 3 --
+
+INSERT INTO `g_facture`
+(`f_numero`, `f_montant_main_oeuvre`, `f_quantite_utilisee`, `f_prix_unitaire`, `f_montant_total`, `f_fk_article_id`, `f_fk_ordre_id`)VALUES
+('FAC-2024-001', 187.50, 2, 45.00, 277.50, 1, 1),
+('FAC-2024-002', 188.50, 2, 46.00, 278.50, 2, 2),
+('FAC-2024-003', 189.50, 2, 47.00, 279.50, 3, 3),
+('FAC-2024-004', 190.50, 2, 48.00, 280.50, 4, 4),
+('FAC-2024-005', 191.50, 2, 49.00, 281.50, 5, 5),
+('FAC-2024-006', 192.50, 2, 50.00, 282.50, 6, 6),
+('FAC-2024-007', 193.50, 2, 51.00, 283.50, 7, 7),
+('FAC-2024-008', 194.50, 2, 52.00, 284.50, 9, 8),
+('FAC-2024-009', 195.50, 2, 53.00, 285.50, 10, 10);
